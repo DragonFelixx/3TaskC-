cbaf578afd7b5da2fc29ad57c14e9ff251ae5440
Velichko Ilya <72504306+DragonFelixx@users.noreply.github.com>
2024-09-02 10:57:29 +0300

    Прикрепил файлы
    Добавление

---
+++ CPP_Task3/CPP_Task3.cpp
@@ -0,0 +1,59 @@
+﻿//Односвязный циклический список (кольцо).
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "CircleList.h"
+
+int main()
+{
+	CircleList<int> *list = new CircleList<int>;
+
+	list->add(1);
+	list->add(2);
+	list->add(3);
+	list->add(5);
+	list->add(7);
+
+	//list->set(0, 6);
+	
+
+	CircleListIterator<int> it = list->begin();
+	while (!(it == list->end()))
+	{
+		cout << *(++it) << endl;
+	}
+
+	CircleListIterator<int> it2 = list->begin();
+	while (!(it2 == list->end()))
+	{
+		cout << *(it2++) << endl;
+	}
+	cout << *it2 << endl;
+
+	delete list;
+
+	CircleList<string>* listString = new CircleList<string>;
+
+	listString->add("Hello");
+	listString->add(", ");
+	listString->add("World");
+
+	listString->remove(", ");
+	listString->set(1, "Morning");
+	listString->set(0, "Good");
+	CircleListIterator<string> it1 = listString->begin();
+	
+
+
+	while (!(it1 == listString->end()))
+	{
+		cout << *it1 << endl;
+
+		it1++;
+	}
+
+	cout << *it1 << endl;
+
+	delete listString;
+}
